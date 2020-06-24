d9dbcc33026bf974af499ccfa07886f11d36d9f3
Samuel Plumppu <greenheartmusic@gmail.com>
2020-06-24 13:11:16 +0200

    feat(header): Add icons and initial menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,43 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, IconButton, Tooltip } from '@material-ui/core'
+import { Tune, GetApp, OpenInBrowser } from '@material-ui/icons'
 
 const Header = () => {
     return (
         <Grid
-            style={{ backgroundColor: 'tomato' }}
+            style={{
+                backgroundColor: 'lightskyblue',
+                width: '100vw',
+                left: 0,
+                top: 0,
+                position: 'fixed',
+            }}
             container
-            justify="center"
+            justify="space-between"
             alignItems="center">
+            <Tooltip title="Change filters" arrow>
+                <IconButton aria-label="Change filters">
+                    <Tune fontSize="large" />
+                </IconButton>
+            </Tooltip>
+
             <Typography variant="h4" component="h1">
                 Swipe for Future Scenario Editor
             </Typography>
+
+            <Grid>
+                <Tooltip title="Open scenario" arrow>
+                    <IconButton aria-label="Open scenario">
+                        <OpenInBrowser fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+
+                <Tooltip title="Download scenario" arrow>
+                    <IconButton aria-label="Download scenario">
+                        <GetApp fontSize="large" />
+                    </IconButton>
+                </Tooltip>
+            </Grid>
         </Grid>
     )
 }
