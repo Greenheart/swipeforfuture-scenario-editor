7bf55e2c1f16f76745c2a51bf674c3a7727e9a84
Samuel Plumppu <greenheartmusic@gmail.com>
2020-06-25 17:24:45 +0200

    feat(theme): Add new color theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core'
-import blue from '@material-ui/core/colors/blue'
-import orange from '@material-ui/core/colors/orange'
 
 import './index.css'
 import App from './App'
 
 const theme = createMuiTheme({
     palette: {
+        common: {
+            black: 'rgba(0, 13, 10, 1)',
+            white: 'rgba(251, 254, 249, 1)',
+        },
+        background: {
+            paper: 'rgba(251, 254, 249, 1)',
+            default: 'rgba(251, 254, 249, 1)',
+        },
         primary: {
-            main: orange[500],
+            light: 'rgba(10, 177, 255, 1)',
+            main: 'rgba(0, 158, 234, 1)',
+            dark: 'rgba(0, 139, 204, 1)',
+            contrastText: 'rgba(251, 254, 249, 1)',
         },
         secondary: {
-            main: blue[500],
+            light: 'rgba(249, 118, 57, 1)',
+            main: 'rgba(248, 102, 36, 1)',
+            dark: 'rgba(237, 80, 7, 1)',
+            contrastText: 'rgba(251, 254, 249, 1)',
+        },
+        error: {
+            light: '#e57373',
+            main: '#f44336',
+            dark: '#d32f2f',
+            contrastText: 'rgba(251, 254, 249, 1)',
+        },
+        text: {
+            primary: 'rgba(0, 13, 10, 1)',
+            secondary: 'rgba(0, 0, 0, 0.54)',
+            disabled: 'rgba(0, 0, 0, 0.38)',
+            hint: 'rgba(0, 0, 0, 0.38)',
         },
     },
 })
